a1f0792778bc20eb615b0f0b7f508826207f2176
XETNDAJ <davidjamesfaustino12@gmail.com>
2025-11-15 20:58:31 +0000

    Started implementing commands

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from PIL import Image
+img = None
 
+while True:
+    try:
+        command = input("> ")
+        words = command.split()
+        match words[0]:
+            case "quit":
+                break
+            case "help":
+                print(''' 
+                quit = exit program
+                help = display this message
+                ''')
+            case "open":
+                img = Image.open(words[1])
+                print(f"Image size: {img.size}" )
+    except Exception as e:
+        print(e)
+            
