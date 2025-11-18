17ae0d343e899d5ecd95e82678e300e9ffe7ac89
XETNDAJ <davidjamesfaustino12@gmail.com>
2025-11-18 02:01:00 +0000

    Save and Resize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageOps
 img = None
+
+print("hello")
 
 while True:
     try:
@@ -14,8 +16,24 @@ while True:
                 help = display this message
                 ''')
             case "open":
-                img = Image.open(words[1])
-                print(f"Image size: {img.size}" )
+                img = Image.open(words[1] + ".jpg")
+                print(f"Image size: {img.size}" ) 
+                img.show()
+            case "save":
+                print(img.filename)
+                if img.filename == words[1] + ".jpg":
+                    why = input("Do you want to overwrite the previous image > ")
+                    if why[0].lower() == "y":
+                        img.save(words[1] + ".jpg")
+                    else:
+                        img.save(words[1] + "_1" + ".jpg")
+                else:
+                    img.save(words[1] + ".jpg")
+            case Resize:
+                x = words[1]
+                y = words[2]
+                img.thumbnail((x,y))
+                
     except Exception as e:
         print(e)
             
