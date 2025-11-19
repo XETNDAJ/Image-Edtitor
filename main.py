24e8703528151968db502b7b367bf2b082d83266
XETNDAJ <davidjamesfaustino12@gmail.com>
2025-11-19 02:55:16 +0000

    Rotate and finish resize

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ while True:
                 print(''' 
                 quit = exit program
                 help = display this message
+                open = open the picture
+                save = save the picture after editing it
+                resize = resize the picture
                 ''')
             case "open":
                 img = Image.open(words[1] + ".jpg")
@@ -29,11 +32,21 @@ while True:
                         img.save(words[1] + "_1" + ".jpg")
                 else:
                     img.save(words[1] + ".jpg")
-            case Resize:
-                x = words[1]
-                y = words[2]
-                img.thumbnail((x,y))
-                
+            case "resize":
+                x =int(words[1])
+                y = int(words[2])
+                #img.thumbnail((x,y))
+                filename = img.filename
+                img = img.resize((x,y))
+                img.filename = filename
+                img.show()
+            case "rotate":
+                degrees = input("How many degrees do you want to rotate it to? > ")
+                degree = int(degrees)
+                filename = img.filename
+                img = img.rotate(degree)
+                img.filename = filename
+            
     except Exception as e:
         print(e)
             
